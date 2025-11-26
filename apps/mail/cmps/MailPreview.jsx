6b385cca4abca633ce352c1d8d54442e660862e2
IshaiPeretz <ishaiperetz@yahoo.com>
2025-11-26 20:51:15 +0200

    buttons colors

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -9,17 +9,15 @@ export function MailPreview({ mail, onRemove, onMarkRead }) {
     const date = utilService.formatCustomDate(mail.sentAt)
 
     const classRead = isRead ? 'read' : ''
-    const iconRead = isRead ? <i class="fa-regular fa-envelope"></i> : <i class="fa-regular fa-envelope-open"></i>
+    const iconRead = isRead ? <i className="fa-regular fa-envelope"></i> : <i className="fa-regular fa-envelope-open"></i>
     return (
         <article className={`mail-prev ${classRead}`}>
-
             <h3>{from}</h3>
             <h3><LongTxt txt={subject} length={30} /></h3>
             <h3> <LongTxt txt={body} length={30} /></h3>
             <h3>{date}</h3>
-            <button onClick={(ev) => { onRemove(ev, mail.id) }}><i class="fa-regular fa-trash-can"></i></button>
+            <button onClick={(ev) => { onRemove(ev, mail.id) }}><i className="fa-regular fa-trash-can"></i></button>
             <button onClick={(ev) => { onMarkRead(ev, mail.id) }}>{iconRead}</button>
-
         </article>
     )
 }
