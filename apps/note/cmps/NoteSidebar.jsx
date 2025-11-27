1d5b5b0fde3ddc1b5533c8e46482bd8d054f2a61
Roy Sonn <roysonnpl@gmail.com>
2025-11-27 20:44:53 +0200

    Implement responsive design

--- apps/note/cmps/NoteSidebar.jsx
+++ apps/note/cmps/NoteSidebar.jsx
@@ -1,20 +1,31 @@
-export function NoteSidebar({ isOpen }) {
+export function NoteSidebar({ isOpen, onClose }) {
   return (
-    <nav className={`note-sidebar ${isOpen ? "open" : ""}`}>
-      <button className="sidebar-item active">
-        <i className="fa-regular fa-lightbulb"></i>
-        <span>Notes</span>
-      </button>
+    <React.Fragment>
+      <div
+        className={`sidebar-overlay ${isOpen ? "show" : ""}`}
+        onClick={onClose}
+      ></div>
 
-      <button className="sidebar-item">
-        <i className="fa-regular fa-pen-to-square"></i>
-        <span>Edit labels</span>
-      </button>
+      <nav className={`note-sidebar ${isOpen ? "open" : ""}`}>
+        <button className="close-sidebar-btn" onClick={onClose}>
+          <i className="fa-solid fa-xmark"></i>
+        </button>
 
-      <button className="sidebar-item">
-        <i className="fa-regular fa-folder-closed"></i>
-        <span>Archive</span>
-      </button>
-    </nav>
+        <button className="sidebar-item active">
+          <i className="fa-regular fa-lightbulb"></i>
+          <span>Notes</span>
+        </button>
+
+        <button className="sidebar-item">
+          <i className="fa-regular fa-pen-to-square"></i>
+          <span>Edit labels</span>
+        </button>
+
+        <button className="sidebar-item">
+          <i className="fa-regular fa-folder-closed"></i>
+          <span>Archive</span>
+        </button>
+      </nav>
+    </React.Fragment>
   );
 }
